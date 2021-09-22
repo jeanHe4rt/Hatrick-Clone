131870ccbc16fcbb27ec33455d9393f9ad4057e0
jeanAt2 <jean0king@gmail.com>
2021-09-22 15:08:40 -0300

    First commit with firsting files config

---
+++ demo/src/main/java/com/hatrick/api/HattrickApiApplication.java
@@ -0,0 +1,13 @@
+package com.hatrick.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HattrickApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HattrickApiApplication.class, args);
+	}
+
+}
